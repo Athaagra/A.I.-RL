a9f5b23ce1d4397013d33f65581d7b4ece4a5d0f
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2023-02-15 13:09:13 -0500

    Add files via upload

--- Q_learning.py
+++ Q_learning.py
@@ -26,37 +26,51 @@ class QLearningAgent:
             if epsilon is None:
                 raise KeyError("Provide an epsilon")
                 
+            if epsilon < sum(self.Q_sa[s][:2]):
             # TO DO: Add own code
-            a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
+                a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
+            else:
+                a=np.argmax(self.Q_sa[s])
             
                 
         elif policy == 'softmax':
             if temp is None:
                 raise KeyError("Provide a temperature")
-                
+            if temp is not None:    
             # TO DO: Add own code
-            a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
-            
+                #a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
+                actions=[np.exp(self.Q_sa[s][i]/temp)/sum(np.exp(self.Q_sa[s]/temp)) for i in range(0,self.n_actions)]
+                actions=np.array(actions)
+                a=np.argmax(actions)
         return a
         
     def update(self,s,a,r,s_next,done):
         # TO DO: Add own code
+        self.Q_sa[s][a]=(1-self.learning_rate)*self.Q_sa[s][a]+self.learning_rate*( r+ self.gamma * max(self.Q_sa[s])-self.Q_sa[s_next][a])
         pass
 
 def q_learning(n_timesteps, learning_rate, gamma, policy='egreedy', epsilon=None, temp=None, plot=True):
     ''' runs a single repetition of q_learning
     Return: rewards, a vector with the observed rewards at each timestep ''' 
-    
+    # TO DO: Write your Q-learning algorithm here! 
     env = StochasticWindyGridworld(initialize_model=False)
     pi = QLearningAgent(env.n_states, env.n_actions, learning_rate, gamma)
     rewards = []
+    state=env.reset()
+    done=False
+    rewards_ep=[]
+    while done!=True:
+        action=pi.select_action(state,policy,epsilon)
+        next_state,reward,done=env.step(action)
+        print(reward)
+        pi.update(state,action,reward,next_state,done)
+        state=next_state
+        rewards_ep.append(reward)
+    rewards=rewards_ep
+    if plot:
+        env.render(Q_sa=pi.Q_sa,plot_optimal_policy=True,step_pause=0.1) # Plot the Q-value estimates during Q-learning execution
 
-    # TO DO: Write your Q-learning algorithm here!
-    
-    # if plot:
-    #    env.render(Q_sa=pi.Q_sa,plot_optimal_policy=True,step_pause=0.1) # Plot the Q-value estimates during Q-learning execution
-
-    return rewards 
+    return rewards
 
 def test():
     
